3de1a881b01823a5c6462cf92f44da162540664f
Grey Pegram <dpegram85@gmail.com>
2024-11-20 19:38:24 -0500

    Completed LAb

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 2, 'M', 'Brown', 'Fiesty', 1),
+('Rowdy', 4, 'M', 'Black', 'Rowdy', 1),
+('Tabitha', 10, 'F', 'Brown', 'Curious', 1),
+('Sergeant Brown', 16, 'M', 'Brown', 'Sleepy', 1),
+('Melissa', 1, 'F', 'Black', 'Happy', 1),
+('Grinch', 5, 'M', 'Green', 'Fiesty', 1),
+('Wendy', 9, 'F', 'Brown', 'Sleepy', 1),
+(NULL, 2, 'M', 'Brown', 'Angry', 1);
